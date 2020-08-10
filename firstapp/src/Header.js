efdc1cd1f87c0bc4030209aa9bfc217a1121353c
aakashdeveloper <ahanda205@gmail.com>
2020-08-10 21:51:05 +0530

    A

---
+++ firstapp/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <React.Fragment>
+            <center>
+                <h2>NareshIT</h2>
+            </center>
+            <hr/>
+        </React.Fragment>
+    )
+}
+
+export default Header;
---
+++ javascript/first.js
@@ -0,0 +1,14 @@
+Es5
+Es6
+Ecma Script 5
+Ecma Script 6
+
+transpile 
+
+//es5
+function add(a,b){
+    return a,b
+}
+
+//es6
+var add = (a,b) => {return a+b}
